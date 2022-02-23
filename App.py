11ce5f935c4f5be922181a0639c02a715ba8aa6d
GourilakshmiKS.com <gourilakshmiks@gmail.com>
2022-02-23 22:33:22 +0530

    first commit

---
+++ App.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template,request,redirect,url_for,flash
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.secret_key="flash message"
+
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='crud'
+
+mysql=MySQL(app)
+
+
+@app.route('/')
+def Index():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/insert',methods=['POST'])
+def insert():
+    if request.method=="POST":
+        flash("Data Inserted Successfully")
+        name=request.form['name']
+        email=request.form['email']
+        phone=request.form['phone']
+
+        cur=mysql.connection.cursor()
+        cur.execute("INSERT INTO students (name,email,phone) values (%s,%s,%s)",(name,email,phone))
+        mysql.connection.commit()
+        return redirect(url_for('Index'))
+               
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
