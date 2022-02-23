dfa81c0022d45ec7a8b456a5ea81fd6780c8790a
GourilakshmiKS.com <gourilakshmiks@gmail.com>
2022-02-24 01:39:56 +0530

    second commit

--- App.py
+++ App.py
@@ -14,7 +14,11 @@ mysql=MySQL(app)
 
 @app.route('/')
 def Index():  # put application's code here
-    return render_template('index.html')
+    cur=mysql.connection.cursor()
+    cur.execute("SELECT * FROM students")
+    data=cur.fetchall()
+    cur.close()
+    return render_template('index.html', students = data)
 
 @app.route('/insert',methods=['POST'])
 def insert():
@@ -28,7 +32,34 @@ def insert():
         cur.execute("INSERT INTO students (name,email,phone) values (%s,%s,%s)",(name,email,phone))
         mysql.connection.commit()
         return redirect(url_for('Index'))
-               
+   
+@app.route('/update',methods=['POST','GET'])
+def update():
+    if request.method=="POST":
+        id_data=request.form['id']
+        
+        name=request.form['name']
+        email=request.form['email']
+        phone=request.form['phone']
+
+        cur=mysql.connection.cursor()
+        cur.execute("""
+            UPDATE students 
+            SET name=%s,email=%s,phone=%s
+            WHERE id=%s
+            """,(name ,email,phone,id_data))
+        flash("Data Updated Successfully")
+        mysql.connection.commit()
+        return redirect(url_for('Index'))    
+
+
+@app.route('/delete/<string:id_data>',methods=['POST','GET'])
+def delete(id_data):
+
+    cur=mysql.connection.cursor()
+    cur.execute("DELETE FROM students WHERE id=%s",(id_data))
+    mysql.connection.commit()
+    return redirect(url_for('Index'))                   
 
 
 if __name__ == '__main__':
